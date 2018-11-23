315b9e431e3c0fb0ba1f9e8d9b300cccd0f3c3bc
xwy27 <xuwy27@mail2.sysu.edu.cn>
2018-11-24 01:17:19 +0800

    Quantization & Distortion

--- Homework2/code/jpeg-compression.py
+++ Homework2/code/jpeg-compression.py
@@ -34,6 +34,38 @@ class CV_YCbCrChannel(Enum):
   Cr = 1
   Cb = 2
 
+class QUANTIFYMATRIX(Enum):
+
+  '''
+  Quantify Matrix Enumerator
+  '''
+
+  LUMINANCE = True
+  CHROMA = False
+
+LUMINANCE_QUANTIFY_MATRIX = [
+  [16,11,10,16,24,40,51,61],
+  [12,12,14,19,26,58,60,55],
+  [14,13,16,24,40,57,69,56],
+  [14,17,22,29,51,87,80,62],
+  [18,22,37,56,68,109,103,77],
+  [24,35,55,64,81,104,113,92],
+  [49,64,78,87,103,121,120,101],
+  [72,92,95,98,112,100,103,99]
+]
+
+CHROMA_QUANTIFY_MATRIX = [
+  [17,18,24,47,99,99,99,99],
+  [18,21,26,66,99,99,99,99],
+  [24,26,56,99,99,99,99,99],
+  [47,66,99,99,99,99,99,99],
+  [99,99,99,99,99,99,99,99],
+  [99,99,99,99,99,99,99,99],
+  [99,99,99,99,99,99,99,99],
+  [99,99,99,99,99,99,99,99]
+]
+
+
 def createMatrix(m,n):
   '''
   Create zero m*n matrix
@@ -41,25 +73,49 @@ def createMatrix(m,n):
   matrix = [[0 for row in range(n)] for col in range(m)]
   return matrix
 
-def toYCbCrSpace(src_img):
+def RGBtoYCbCrSpace(img):
   '''
-  Transform src_img into YCbCr color space image.
-  src_img is in BGR mode and returns in YCrCb mode,
+  Transform img from RGB to YCbCr color space image.
+  img is in BGR mode and returns in YCrCb mode,
   which keeps the same in opencv
   '''
-  row = src_img.shape[0]
-  col = src_img.shape[1]
+  row = img.shape[0]
+  col = img.shape[1]
 
   for x in range(0, row):
     for y in range(0, col):
-      r = src_img.item(x, y, CV_RGBChannel.RED.value)
-      g = src_img.item(x, y, CV_RGBChannel.GREEN.value)
-      b = src_img.item(x, y, CV_RGBChannel.BLUE.value)
-      src_img.itemset((x, y, CV_YCbCrChannel.Y.value), 0.299*r+0.587*g+0.114*b)
-      src_img.itemset((x, y, CV_YCbCrChannel.Cb.value), -0.168736*r-0.331264*g+0.5*b + 0.5 + 128)
-      src_img.itemset((x, y, CV_YCbCrChannel.Cr.value), 0.5*r-0.418688*g-0.081312*b + 0.5 + 128)
+      r = img.item(x, y, CV_RGBChannel.RED.value)
+      g = img.item(x, y, CV_RGBChannel.GREEN.value)
+      b = img.item(x, y, CV_RGBChannel.BLUE.value)
+      img.itemset((x, y, CV_YCbCrChannel.Y.value), 0.299*r+0.587*g+0.114*b)
+      img.itemset((x, y, CV_YCbCrChannel.Cb.value), -0.1687*r-0.3313*g+0.5*b + 128)
+      img.itemset((x, y, CV_YCbCrChannel.Cr.value), 0.5*r-0.418*g-0.0813*b + 128)
 
-  return src_img
+  return img
+
+def YCbCrtoRGBSpace(img):
+  '''
+  Transform img from YCbCr to RGB color space image.
+  img is in BGR mode and returns in YCrCb mode,
+  which keeps the same in opencv
+  '''
+  row = img.shape[0]
+  col = img.shape[1]
+
+  for x in range(0, row):
+    for y in range(0, col):
+      Y = img.item(x, y, CV_YCbCrChannel.Y.value)
+      Cb = img.item(x, y, CV_YCbCrChannel.Cb.value) - 128
+      Cr = img.item(x, y, CV_YCbCrChannel.Cr.value) - 128
+
+      r = Y+1.402*Cr if (Y+1.402*Cr > 0) else 0
+      g = Y-0.34414*Cb-0.71414*Cr if (Y-0.34414*Cb-0.71414*Cr > 0) else 0
+      b = Y+1.772*Cb if (Y+1.772*Cb > 0) else 0
+      img.itemset((x, y, CV_RGBChannel.RED.value), r)
+      img.itemset((x, y, CV_RGBChannel.GREEN.value), g)
+      img.itemset((x, y, CV_RGBChannel.BLUE.value), b)
+
+  return img
 
 def chromaSample(img):
   '''
@@ -88,7 +144,7 @@ def chromaSample(img):
 
 def DCT_1D_COL(block):
   '''
-  Calculate G(i,v)
+  Calculate G(i,v) from f(i, j)
   '''
   S = createMatrix(8, 8)
   for i in range(0, 8):
@@ -102,7 +158,7 @@ def DCT_1D_COL(block):
 
 def DCT_1D_ROW(block):
   '''
-  Calculate F(u,v)
+  Calculate F(u,v) from G(i, v)
   '''
   S = createMatrix(8, 8)
   for u in range(0, 8):
@@ -115,45 +171,51 @@ def DCT_1D_ROW(block):
   return S
 
 def IDCT_1D_ROW(matrix):
-		S = createMatrix(8,8)
-		for row in range(8):
-			for i in range(8):
-				temp = 0
-				for u in range(8):
-					c = math.sqrt(2)/2 if(u==0) else 1
-					temp += c*math.cos((2*i+1)*u*math.pi/16)*matrix[row][u]/2
-				S[row][i] = round(temp)
-		return S
-
-def IDCT_1D_COL(matrix):
+  '''
+  Calculate G(i,v) from F(u,v)
+  '''
   S = createMatrix(8,8)
-  for col in range(8):
-    for i in range(8):
+  for i in range(0, 8):
+    for v in range(0, 8):
       temp = 0
-      for u in range(8):
+      for u in range(0, 8):
         c = math.sqrt(2)/2 if(u==0) else 1
-        temp += c*math.cos((2*i+1)*u*math.pi/16)*matrix[u][col]/2
-      S[i][col] = round(temp) + 128
+        temp += c/2*math.cos((2*i+1)*u*math.pi/16)*matrix[u][v]
+      S[i][v] = round(temp)
   return S
 
-def DCT_img(img):
+def IDCT_1D_COL(matrix):
+  '''
+  Calculate f(i,j) from G(i,v)
+  '''
+  S = createMatrix(8,8)
+  for i in range(0, 8):
+    for j in range(0, 8):
+      temp = 0
+      for v in range(0, 8):
+        c = math.sqrt(2)/2 if(v==0) else 1
+        temp += c/2*math.cos((2*j+1)*v*math.pi/16)*matrix[i][v]
+      S[i][j] = round(temp) + 128
+  return S
+
+def DCTImg(img):
+  '''
+  Perform DCT on image
+  '''
   row = img.shape[0]
   col = img.shape[1]
   x = 0
   y = 0
   while (x < row - 7):
     while (y < col - 7):
-      print('\n\n')
       y_matrix = createMatrix(8, 8)
       cb_matrix = createMatrix(8, 8)
       cr_matrix = createMatrix(8, 8)
-      print('-' * 15, "DCT START", '-' * 15)
       for i in range(0, 8):
         for j in range(0, 8):
-          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value) - 128
-          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value) - 128
-          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value) - 128
-        print(y_matrix[i])
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)-128
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)-128
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)-128
       y_matrix = DCT_1D_COL(y_matrix)
       cb_matrix = DCT_1D_COL(cb_matrix)
       cr_matrix = DCT_1D_COL(cr_matrix)
@@ -162,15 +224,33 @@ def DCT_img(img):
       cb_matrix = DCT_1D_ROW(cb_matrix)
       cr_matrix = DCT_1D_ROW(cr_matrix)
 
-      print('-' * 15, "DCT DONE", '-' * 15)
       for i in range(0, 8):
-        print(y_matrix[i])
         for j in range(0, 8):
           img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
           img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
           img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
-          # print(img.item(x + i, y + j, CV_YCbCrChannel.Y.value))
+      y += 8
+    x += 8
+  return img
 
+def IDCTImg(img):
+  '''
+  Perform IDCT on image
+  '''
+  row = img.shape[0]
+  col = img.shape[1]
+  x = 0
+  y = 0
+  while (x < row - 7):
+    while (y < col - 7):
+      y_matrix = createMatrix(8, 8)
+      cb_matrix = createMatrix(8, 8)
+      cr_matrix = createMatrix(8, 8)
+      for i in range(0, 8):
+        for j in range(0, 8):
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
       y_matrix = IDCT_1D_ROW(y_matrix)
       cb_matrix = IDCT_1D_ROW(cb_matrix)
       cr_matrix = IDCT_1D_ROW(cr_matrix)
@@ -178,38 +258,180 @@ def DCT_img(img):
       y_matrix = IDCT_1D_COL(y_matrix)
       cb_matrix = IDCT_1D_COL(cb_matrix)
       cr_matrix = IDCT_1D_COL(cr_matrix)
-      print('-' * 15, "IDCT DONE", '-' * 15)
+
       for i in range(0, 8):
-        print(y_matrix[i])
         for j in range(0, 8):
           img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
           img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
           img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
-
       y += 8
     x += 8
   return img
 
-# origin = cv.imread('../img/cartoon.jpg', cv.IMREAD_COLOR)
-# cv.imshow('origin', origin)
+def Quantify(matrix, quantifyMatrix):
+  '''
+  Quantify matrix with given quantifyMatrix
+  quantifyMatrix = true means Luminance Quantify
+  quantifyMatrix = false means Chroma Quantify
+  '''
+  S = createMatrix(8, 8)
+  if (quantifyMatrix == QUANTIFYMATRIX.LUMINANCE.value):
+    for i in range(0, 8):
+      for j in range(0, 8):
+        temp = matrix[i][j] / LUMINANCE_QUANTIFY_MATRIX[i][j]
+        S[i][j] = (int)(round(temp))
+  else: # QUANTIFYMATRIX.CHROMA.value
+    for i in range(0, 8):
+      for j in range(0, 8):
+        temp = matrix[i][j] / CHROMA_QUANTIFY_MATRIX[i][j]
+        S[i][j] = (int)(round(temp))
+  return S
 
+def DeQuantify(matrix, quantifyMatrix):
+  '''
+  DeQuantify matrix with given quantifyMatrix
+  quantifyMatrix = true means Luminance Quantify
+  quantifyMatrix = false means Chroma Quantify
+  '''
+  S = createMatrix(8, 8)
+  if (quantifyMatrix == QUANTIFYMATRIX.LUMINANCE.value):
+    for i in range(0, 8):
+      for j in range(0, 8):
+        S[i][j] = matrix[i][j] * LUMINANCE_QUANTIFY_MATRIX[i][j]
+  else: # QUANTIFYMATRIX.CHROMA.value
+    for i in range(0, 8):
+      for j in range(0, 8):
+        S[i][j] = matrix[i][j] * CHROMA_QUANTIFY_MATRIX[i][j]
+  return S
+
+def QuantifyImg(img):
+  '''
+  Perform quantify on image
+  '''
+  row = img.shape[0]
+  col = img.shape[1]
+  x = 0
+  y = 0
+  while (x < row - 7):
+    while (y < col - 7):
+      y_matrix = createMatrix(8, 8)
+      cb_matrix = createMatrix(8, 8)
+      cr_matrix = createMatrix(8, 8)
+      for i in range(0, 8):
+        for j in range(0, 8):
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+      y_matrix = Quantify(y_matrix, QUANTIFYMATRIX.LUMINANCE)
+      cb_matrix = Quantify(cb_matrix, QUANTIFYMATRIX.CHROMA)
+      cr_matrix = Quantify(cr_matrix, QUANTIFYMATRIX.CHROMA)
+
+      for i in range(0, 8):
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+      y += 8
+    x += 8
+  return img
+
+def DeQuantifyImg(img):
+  '''
+  Perform dequantify on image
+  '''
+  row = img.shape[0]
+  col = img.shape[1]
+  x = 0
+  y = 0
+  while (x < row - 7):
+    while (y < col - 7):
+      y_matrix = createMatrix(8, 8)
+      cb_matrix = createMatrix(8, 8)
+      cr_matrix = createMatrix(8, 8)
+      for i in range(0, 8):
+        for j in range(0, 8):
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+      y_matrix = DeQuantify(y_matrix, QUANTIFYMATRIX.LUMINANCE)
+      cb_matrix = DeQuantify(cb_matrix, QUANTIFYMATRIX.CHROMA)
+      cr_matrix = DeQuantify(cr_matrix, QUANTIFYMATRIX.CHROMA)
+
+      for i in range(0, 8):
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+      y += 8
+    x += 8
+  return img
+
+def LossDegree(src, res):
+  '''
+  Calculate the distortion of RGB
+  '''
+  R_loss = 0
+  G_loss = 0
+  B_loss = 0
+  R_snr = 0
+  G_snr = 0
+  B_snr = 0
+  row = src.shape[0]
+  col = src.shape[1]
+  for i in range(0, row):
+    for j in range(0, col):
+      R_loss += math.pow(src.item(i, j, CV_RGBChannel.RED.value) - res.item(i, j, CV_RGBChannel.RED.value), 2)
+      R_snr += math.pow(src.item(i, j, CV_RGBChannel.RED.value), 2)
+      G_loss += math.pow(src.item(i, j, CV_RGBChannel.GREEN.value) - res.item(i, j, CV_RGBChannel.GREEN.value), 2)
+      G_snr += math.pow(src.item(i, j, CV_RGBChannel.GREEN.value), 2)
+      B_loss += math.pow(src.item(i, j, CV_RGBChannel.BLUE.value) - res.item(i, j, CV_RGBChannel.BLUE.value), 2)
+      B_snr += math.pow(src.item(i, j, CV_RGBChannel.BLUE.value), 2)
+      # print(src.item(i, j, CV_RGBChannel.BLUE.value), res.item(i, j, CV_RGBChannel.BLUE.value))
+  R_snr = 10*math.log10(R_snr / R_loss)
+  G_snr = 10*math.log10(G_snr / G_loss)
+  B_snr = 10*math.log10(B_snr / B_loss)
+  R_loss = round(R_loss / (row * col))
+  G_loss = round(G_loss / (row * col))
+  B_loss = round(B_loss / (row * col))
+  return R_loss, R_snr, G_loss, G_snr, B_loss, B_snr
+
+# Origin photo
 img = cv.imread('../img/cartoon.jpg', cv.IMREAD_COLOR)
 img = cv.resize(img,(1000,720),interpolation = cv.INTER_CUBIC)
+img_c = copy.deepcopy(img)
+# img_copy = cv.cvtColor(img_c, cv.COLOR_BGR2YCR_CB)
 
-# Library YCbCr
-# lib_ycbcr = cv.cvtColor(img,cv.COLOR_BGR2YCrCb)
 # Transform rgb to YCbCr
-my_ycbcr = toYCbCrSpace(img)
-# Sample image
-img_sample = chromaSample(my_ycbcr)
-img_copy = copy.deepcopy(img_sample)
+my_ycbcr = RGBtoYCbCrSpace(img)
+# idct_img = RGBtoYCbCrSpace(img)
 
-dct_img = TEST_DCT_IDCT(img_sample)
+# Chroma sample image
+sample_img = chromaSample(my_ycbcr)
+# idct_img = chromaSample(my_ycbcr)
 
+# DCT image
+dct_img = DCTImg(sample_img)
 
+# # Quantify image
+quant_img = QuantifyImg(dct_img)
 
-# cv.imshow('my_ycbcr', my_ycbcr)
-# cv.imshow('IDCT_img', img_sample)
-# cv.imshow('myYCbCr_img', img_copy)
+# # Dequantify image
+dequant_img = DeQuantifyImg(quant_img)
+
+# IDCT image
+idct_img = IDCTImg(dequant_img)
+# idct_img = IDCTImg(dct_img)
+
+# # reconstruct image
+reconstruct_img = YCbCrtoRGBSpace(idct_img)
+
+a,A,b,B,c,C=LossDegree(img_c, reconstruct_img)
+print 'cartoon.jpg'
+print 'R Channel MSE: ', a, 'R Channel SNR: ', A
+print 'G Channel MSE: ', b, 'G Channel SNR: ', B
+print 'B Channel MSE: ', c, 'B Channel SNR: ', C
+
+# cv.imshow('my_img', reconstruct_img)
+# cv.imshow('origin_img', img_c)
 # cv.waitKey(0)
 # cv.destroyAllWindows()
---
+++ Homework2/code/test.py
@@ -0,0 +1,206 @@
+def TEST_DCT_IDCT(img):
+  row = img.shape[0]
+  col = img.shape[1]
+  x = 0
+  y = 0
+  while (x < row - 7):
+    while (y < col - 7):
+      print('\n\n')
+      y_matrix = createMatrix(8, 8)
+      cb_matrix = createMatrix(8, 8)
+      cr_matrix = createMatrix(8, 8)
+      print('-' * 15, "DCT START", '-' * 15)
+      for i in range(0, 8):
+        for j in range(0, 8):
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value) - 128
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value) - 128
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value) - 128
+        print(y_matrix[i])
+      y_matrix = DCT_1D_COL(y_matrix)
+      cb_matrix = DCT_1D_COL(cb_matrix)
+      cr_matrix = DCT_1D_COL(cr_matrix)
+      
+      y_matrix = DCT_1D_ROW(y_matrix)
+      cb_matrix = DCT_1D_ROW(cb_matrix)
+      cr_matrix = DCT_1D_ROW(cr_matrix)
+
+      print('-' * 15, "DCT DONE", '-' * 15)
+      for i in range(0, 8):
+        print(y_matrix[i])
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+          # print(img.item(x + i, y + j, CV_YCbCrChannel.Y.value))
+
+      y_matrix = IDCT_1D_ROW(y_matrix)
+      cb_matrix = IDCT_1D_ROW(cb_matrix)
+      cr_matrix = IDCT_1D_ROW(cr_matrix)
+      
+      y_matrix = IDCT_1D_COL(y_matrix)
+      cb_matrix = IDCT_1D_COL(cb_matrix)
+      cr_matrix = IDCT_1D_COL(cr_matrix)
+      print('-' * 15, "IDCT DONE", '-' * 15)
+      for i in range(0, 8):
+        print(y_matrix[i])
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+
+      y += 8
+    x += 8
+  return img
+
+def TEST_QUANTIFY_DEQUANTIFY(img):
+  row = img.shape[0]
+  col = img.shape[1]
+  x = 0
+  y = 0
+  while (x < row - 7):
+    print('\n\n')
+    while (y < col - 7):
+      y_matrix = createMatrix(8, 8)
+      cb_matrix = createMatrix(8, 8)
+      cr_matrix = createMatrix(8, 8)
+      print(15 * '-' + 'QUANTIFY START' + 15 * '-')
+      for i in range(0, 8):
+        for j in range(0, 8):
+          y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+          cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+          cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+        print(y_matrix[i])
+      y_matrix = Quantify(y_matrix, QUANTIFYMATRIX.LUMINANCE)
+      cb_matrix = Quantify(cb_matrix, QUANTIFYMATRIX.CHROMINANCE)
+      cr_matrix = Quantify(cr_matrix, QUANTIFYMATRIX.CHROMINANCE)
+
+      print(15 * '-' + 'QUANTIFY DONE' + 15 * '-')
+      for i in range(0, 8):
+        print(y_matrix[i])
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+      y_matrix = DeQuantify(y_matrix, QUANTIFYMATRIX.LUMINANCE)
+      cb_matrix = DeQuantify(cb_matrix, QUANTIFYMATRIX.CHROMINANCE)
+      cr_matrix = DeQuantify(cr_matrix, QUANTIFYMATRIX.CHROMINANCE)
+
+      print(15 * '-' + 'DEQUANTIFY DONE' + 15 * '-')
+      for i in range(0, 8):
+        print(y_matrix[i])
+        for j in range(0, 8):
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+          img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+      y += 8
+    x += 8
+  return img
+
+# def TEST_DCT_IDCT(img):
+#   # test = cv.imread('../img/animal.jpg', cv.IMREAD_COLOR)
+#   # test = cv.resize(test,(1000,720),interpolation = cv.INTER_CUBIC)
+#   # test = cv.cvtColor(test, cv.COLOR_BGR2YCR_CB)
+#   # test = test.astype('float')
+#   # img_dct=cv.CreateMat(test.rows,test.cols,cv.CV_32FC1)
+#   # test = cv.dct(test, img_dct)
+#   row = img.shape[0]
+#   col = img.shape[1]
+#   x = 0
+#   y = 0
+#   while (x < row - 7):
+#     while (y < col - 7):
+#       print('\n\n')
+#       y_matrix = createMatrix(8, 8)
+#       cb_matrix = createMatrix(8, 8)
+#       cr_matrix = createMatrix(8, 8)
+#       oy_matrix = createMatrix(8, 8)
+#       ocb_matrix = createMatrix(8, 8)
+#       ocr_matrix = createMatrix(8, 8)
+#       print('-' * 15, "DCT START", '-' * 15)
+#       for i in range(0, 8):
+#         for j in range(0, 8):
+#           y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)-128
+#           cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)-128
+#           cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)-128
+#           # oy_matrix[i][j] = test.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+#           # ocb_matrix[i][j] = test.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+#           # ocr_matrix[i][j] = test.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+#         print(y_matrix[i])
+#       y_matrix = DCT_1D_COL(y_matrix)
+#       cb_matrix = DCT_1D_COL(cb_matrix)
+#       cr_matrix = DCT_1D_COL(cr_matrix)
+      
+#       y_matrix = DCT_1D_ROW(y_matrix)
+#       cb_matrix = DCT_1D_ROW(cb_matrix)
+#       cr_matrix = DCT_1D_ROW(cr_matrix)
+
+#       print('-' * 15, "DCT DONE", '-' * 15)
+#       for i in range(0, 8):
+#         # print('+' * 5, "DCT-My", '+' * 5)
+#         print(y_matrix[i])
+#         # print('+' * 5, "DCT-Lib", '+' * 5)
+#         # print(oy_matrix[i])
+#         for j in range(0, 8):
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+#           # print(img.item(x + i, y + j, CV_YCbCrChannel.Y.value))
+#           y_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+#           cb_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+#           cr_matrix[i][j] = img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+
+#       y_matrix = IDCT_1D_ROW(y_matrix)
+#       cb_matrix = IDCT_1D_ROW(cb_matrix)
+#       cr_matrix = IDCT_1D_ROW(cr_matrix)
+      
+#       y_matrix = IDCT_1D_COL(y_matrix)
+#       cb_matrix = IDCT_1D_COL(cb_matrix)
+#       cr_matrix = IDCT_1D_COL(cr_matrix)
+#       print('-' * 15, "IDCT DONE", '-' * 15)
+#       for i in range(0, 8):
+#         print(y_matrix[i])
+#         for j in range(0, 8):
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Y.value), y_matrix[i][j])
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Cb.value), cb_matrix[i][j])
+#           img.itemset((x + i, y + j, CV_YCbCrChannel.Cr.value), cr_matrix[i][j])
+
+#       y += 8
+#     x += 8
+#   return img
+# cv.imshow('dct', TEST_DCT_IDCT(sample_img))
+
+# row = img.shape[0]
+# col = img.shape[1]
+# x = 0
+# y = 0
+# while (x < row - 7):
+#   while (y < col - 7):
+#     y_matrix = createMatrix(8, 8)
+#     cb_matrix = createMatrix(8, 8)
+#     cr_matrix = createMatrix(8, 8)
+#     oy_matrix = createMatrix(8, 8)
+#     ocb_matrix = createMatrix(8, 8)
+#     ocr_matrix = createMatrix(8, 8)
+#     for i in range(0, 8):
+#       for j in range(0, 8):
+#         y_matrix[i][j] = idct_img.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+#         cb_matrix[i][j] = idct_img.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+#         cr_matrix[i][j] = idct_img.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+#         oy_matrix[i][j] = img_copy.item(x + i, y + j, CV_YCbCrChannel.Y.value)
+#         ocb_matrix[i][j] = img_copy.item(x + i, y + j, CV_YCbCrChannel.Cb.value)
+#         ocr_matrix[i][j] = img_copy.item(x + i, y + j, CV_YCbCrChannel.Cr.value)
+#       print(15 * '-' + 'My-Y' + 15 * '-')
+#       print(y_matrix[i])
+#       print(15 * '-' + 'Origin-Y' + 15 * '-')
+#       print(oy_matrix[i])
+#       print(15 * '-' + 'My-Cb' + 15 * '-')
+#       print(cb_matrix[i])
+#       print(15 * '-' + 'Origin-Cb' + 15 * '-')
+#       print(ocb_matrix[i])
+#       print(15 * '-' + 'My-Cr' + 15 * '-')
+#       print(cr_matrix[i])
+#       print(15 * '-' + 'Origin-Cr' + 15 * '-')
+#       print(ocr_matrix[i])
+    
+#     y += 8
+#   x += 8
